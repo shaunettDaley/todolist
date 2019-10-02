330dc1bc0eff12fdd3660c94f4cfbf742cb40cd6
shaunettDaley <54638418+shaunettDaley@users.noreply.github.com>
2019-10-01 21:18:07 -0400

    Update  variable name from todolist to todoslist

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ filterTodos(term);
 
 
 // strike out item
-todoList.addEventListener('change', e => {
+todosList.addEventListener('change', e => {
     if(e.target.checked){
 e.target.nextElementSibling.style.textDecoration = "line-through";
     }else{
